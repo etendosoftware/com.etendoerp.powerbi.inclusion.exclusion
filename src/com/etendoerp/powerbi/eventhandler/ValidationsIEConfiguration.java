c8584ef631130c790ee498bb13c53de036bdfe64
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2022-03-16 15:35:51 -0300

    LA-2066: Correcciones de ventana, checks de cabecera, validacion y categoria de config

---
+++ src/com/etendoerp/powerbi/eventhandler/ValidationsIEConfiguration.java
@@ -0,0 +1,55 @@
+package com.etendoerp.powerbi.eventhandler;
+
+import com.etendoerp.powerbi.data.IEConfiguration;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.openbravo.base.exception.OBException;
+import org.openbravo.base.model.Entity;
+import org.openbravo.base.model.ModelProvider;
+import org.openbravo.client.kernel.event.EntityNewEvent;
+import org.openbravo.client.kernel.event.EntityPersistenceEventObserver;
+import org.openbravo.client.kernel.event.EntityUpdateEvent;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.database.ConnectionProvider;
+import org.openbravo.erpCommon.utility.OBMessageUtils;
+import org.openbravo.service.db.DalConnectionProvider;
+
+import javax.enterprise.event.Observes;
+
+//mensaje etbi_config_need_checks
+public class ValidationsIEConfiguration extends EntityPersistenceEventObserver {
+  private static Entity[] entities = {
+      ModelProvider.getInstance().getEntity(IEConfiguration.ENTITY_NAME) };
+  protected Logger logger = Logger.getLogger(this.getClass());
+  private static final String LANGUAGE = OBContext.getOBContext().getLanguage().getLanguage();
+  private static final ConnectionProvider conn = new DalConnectionProvider(false);
+
+  @Override protected Entity[] getObservedEntities() {
+    return entities;
+  }
+
+  public void onUpdate(@Observes EntityUpdateEvent event) {
+    if (!isValidEvent(event)) {
+      return;
+    }
+    //if the type of the configuration is null(using the method is empty from stringutils), the hasstring or the hasnumer or the hasyesno must be true
+    IEConfiguration config = (IEConfiguration) event.getTargetInstance();
+    if (StringUtils.isEmpty(
+        config.getType()) && !config.isHasstring() && !config.isHasnumber() && !config.isHasyesno()) {
+      throw new OBException(OBMessageUtils.messageBD("etbi_config_need_checks"));
+    }
+  }
+  public void onSave(@Observes EntityNewEvent event) {
+      if (!isValidEvent(event)) {
+        return;
+      }
+      //if the type of the configuration is null(using the method is empty from stringutils), the hasstring or the hasnumer or the hasyesno must be true
+      IEConfiguration config = (IEConfiguration) event.getTargetInstance();
+      if (StringUtils.isEmpty(
+          config.getType()) && !config.isHasstring() && !config.isHasnumber() && !config.isHasyesno()) {
+        throw new OBException(OBMessageUtils.messageBD("etbi_config_need_checks"));
+      }
+    }
+
+}
+
